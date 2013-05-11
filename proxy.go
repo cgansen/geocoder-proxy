dac031d9ebef2e8c5822d040e2ae7fa8b0020ac6
Chris Gansen <cgansen@gmail.com>
2013-05-11 14:14:21 -0500

    wip

--- proxy.go
+++ proxy.go
@@ -3,23 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+        "net/url"
         "io/ioutil"
         "log"
 )
 
+const GMapsHost = "http://maps.googleapis.com/maps/api/geocode/"
+const GMapsResponseFormat = "json"        
+
 type GeocoderProxy struct{}
+
 type GoogleMapsRequest struct {}
 
 func (gp GeocoderProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
         var gmr GoogleMapsRequest
         fmt.Println(r.URL)
-        resp := gmr.Get("http://example.com" + r.URL.String())
+
+        gmaps_url := url.URL{Scheme: "https", Host: "maps.googleapis.com", Path: "/maps/api/geocode/json" }         
+        q := gmaps_url.Query()
+        q.Set("address", r.FormValue("address"))
+        gmaps_url.RawQuery = q.Encode()
         
-        fmt.Println(resp)
+        gmr.Get(gmaps_url.String())
+        // fmt.Println(gmaps_url.String())
 }
 
 func (gmr *GoogleMapsRequest) Get(url string) (response string) {
         // TODO: sign request
+        fmt.Println(url)
+        
         resp, err := http.Get(url)
         
         body, err := ioutil.ReadAll(resp.Body)
